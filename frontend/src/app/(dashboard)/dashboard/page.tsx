bc3af5bf118f21225827d029aff950284825a7f6
Emmy Gretta <karabogretta@gmail.com>
2025-05-21 11:45:12 +0200

    Adding .env

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
 
         <div
           className={`grid gap-6 ${
-            isAdmin ? "grid-cols-1 md:grid-cols-3" : "grid-cols-1 md:grid-cols-2"
+            isAdmin ? "grid-cols-1 md:grid-rows-3" : "grid-cols-1 md:grid-rows-2"
           }`}
         >
           {/* Cards for Admin */}
